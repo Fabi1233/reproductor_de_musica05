53a79a6759c945b9dccbca352b0402bea66f9208
Fabi1233 <Fabi1233@users.noreply.github.com>
2016-09-07 10:21:56 -0500

    Add files via upload

---
+++ Form1.Designer.vb
@@ -0,0 +1,431 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.TableLayoutPanel1 = New System.Windows.Forms.TableLayoutPanel()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.btnVer = New System.Windows.Forms.Button()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.Label9 = New System.Windows.Forms.Label()
+        Me.Button5 = New System.Windows.Forms.Button()
+        Me.Button6 = New System.Windows.Forms.Button()
+        Me.Label10 = New System.Windows.Forms.Label()
+        Me.Label11 = New System.Windows.Forms.Label()
+        Me.Label12 = New System.Windows.Forms.Label()
+        Me.Button7 = New System.Windows.Forms.Button()
+        Me.Button8 = New System.Windows.Forms.Button()
+        Me.Label13 = New System.Windows.Forms.Label()
+        Me.Label14 = New System.Windows.Forms.Label()
+        Me.Label15 = New System.Windows.Forms.Label()
+        Me.Button9 = New System.Windows.Forms.Button()
+        Me.Button10 = New System.Windows.Forms.Button()
+        Me.tlpHeader = New System.Windows.Forms.TableLayoutPanel()
+        Me.TableLayoutPanel3 = New System.Windows.Forms.TableLayoutPanel()
+        Me.lblTitulo = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.lblArtista = New System.Windows.Forms.Label()
+        Me.lblPrecio = New System.Windows.Forms.Label()
+        Me.pbCover = New System.Windows.Forms.ImageList(Me.components)
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.TableLayoutPanel1.SuspendLayout()
+        Me.tlpHeader.SuspendLayout()
+        Me.TableLayoutPanel3.SuspendLayout()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'TableLayoutPanel1
+        '
+        Me.TableLayoutPanel1.ColumnCount = 5
+        Me.TableLayoutPanel1.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 7.643058!))
+        Me.TableLayoutPanel1.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 49.27971!))
+        Me.TableLayoutPanel1.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 11.23449!))
+        Me.TableLayoutPanel1.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 15.92637!))
+        Me.TableLayoutPanel1.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 15.91637!))
+        Me.TableLayoutPanel1.Controls.Add(Me.Label4, 0, 1)
+        Me.TableLayoutPanel1.Controls.Add(Me.Label6, 2, 1)
+        Me.TableLayoutPanel1.Controls.Add(Me.btnVer, 3, 1)
+        Me.TableLayoutPanel1.Controls.Add(Me.Button4, 4, 1)
+        Me.TableLayoutPanel1.Controls.Add(Me.Label7, 0, 2)
+        Me.TableLayoutPanel1.Controls.Add(Me.Label8, 1, 2)
+        Me.TableLayoutPanel1.Controls.Add(Me.Label9, 2, 2)
+        Me.TableLayoutPanel1.Controls.Add(Me.Button5, 3, 2)
+        Me.TableLayoutPanel1.Controls.Add(Me.Button6, 4, 2)
+        Me.TableLayoutPanel1.Controls.Add(Me.Label10, 0, 3)
+        Me.TableLayoutPanel1.Controls.Add(Me.Label11, 1, 3)
+        Me.TableLayoutPanel1.Controls.Add(Me.Label12, 2, 3)
+        Me.TableLayoutPanel1.Controls.Add(Me.Button7, 3, 3)
+        Me.TableLayoutPanel1.Controls.Add(Me.Button8, 4, 3)
+        Me.TableLayoutPanel1.Controls.Add(Me.Label13, 0, 4)
+        Me.TableLayoutPanel1.Controls.Add(Me.Label14, 1, 4)
+        Me.TableLayoutPanel1.Controls.Add(Me.Label15, 2, 4)
+        Me.TableLayoutPanel1.Controls.Add(Me.Button9, 3, 4)
+        Me.TableLayoutPanel1.Controls.Add(Me.Button10, 4, 4)
+        Me.TableLayoutPanel1.Controls.Add(Me.Label5, 1, 1)
+        Me.TableLayoutPanel1.Controls.Add(Me.Label2, 1, 0)
+        Me.TableLayoutPanel1.Location = New System.Drawing.Point(12, 208)
+        Me.TableLayoutPanel1.Name = "TableLayoutPanel1"
+        Me.TableLayoutPanel1.RowCount = 5
+        Me.TableLayoutPanel1.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50.0!))
+        Me.TableLayoutPanel1.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50.0!))
+        Me.TableLayoutPanel1.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 37.0!))
+        Me.TableLayoutPanel1.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 38.0!))
+        Me.TableLayoutPanel1.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 36.0!))
+        Me.TableLayoutPanel1.Size = New System.Drawing.Size(365, 244)
+        Me.TableLayoutPanel1.TabIndex = 0
+        '
+        'Label4
+        '
+        Me.Label4.Anchor = System.Windows.Forms.AnchorStyles.None
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(7, 92)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(13, 13)
+        Me.Label4.TabIndex = 5
+        Me.Label4.Text = "1"
+        '
+        'Label5
+        '
+        Me.Label5.Anchor = System.Windows.Forms.AnchorStyles.Left
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(30, 92)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(119, 13)
+        Me.Label5.TabIndex = 6
+        Me.Label5.Text = "Adventure Of A Lifetime"
+        '
+        'Label6
+        '
+        Me.Label6.Anchor = System.Windows.Forms.AnchorStyles.None
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(212, 92)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(28, 13)
+        Me.Label6.TabIndex = 7
+        Me.Label6.Text = "4:25"
+        '
+        'btnVer
+        '
+        Me.btnVer.Location = New System.Drawing.Point(250, 69)
+        Me.btnVer.Name = "btnVer"
+        Me.btnVer.Size = New System.Drawing.Size(52, 23)
+        Me.btnVer.TabIndex = 8
+        Me.btnVer.Text = "Ver"
+        Me.btnVer.UseVisualStyleBackColor = True
+        '
+        'Button4
+        '
+        Me.Button4.Location = New System.Drawing.Point(308, 69)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(54, 23)
+        Me.Button4.TabIndex = 9
+        Me.Button4.Text = "Comprar"
+        Me.Button4.UseVisualStyleBackColor = True
+        '
+        'Label7
+        '
+        Me.Label7.Anchor = System.Windows.Forms.AnchorStyles.None
+        Me.Label7.AutoSize = True
+        Me.Label7.Location = New System.Drawing.Point(7, 144)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(13, 13)
+        Me.Label7.TabIndex = 10
+        Me.Label7.Text = "2"
+        '
+        'Label8
+        '
+        Me.Label8.Anchor = System.Windows.Forms.AnchorStyles.Left
+        Me.Label8.AutoSize = True
+        Me.Label8.Location = New System.Drawing.Point(30, 144)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(106, 13)
+        Me.Label8.TabIndex = 11
+        Me.Label8.Text = "Sweet Child On Mine"
+        '
+        'Label9
+        '
+        Me.Label9.Anchor = System.Windows.Forms.AnchorStyles.None
+        Me.Label9.AutoSize = True
+        Me.Label9.Location = New System.Drawing.Point(212, 144)
+        Me.Label9.Name = "Label9"
+        Me.Label9.Size = New System.Drawing.Size(28, 13)
+        Me.Label9.TabIndex = 12
+        Me.Label9.Text = "3:56"
+        '
+        'Button5
+        '
+        Me.Button5.Location = New System.Drawing.Point(250, 135)
+        Me.Button5.Name = "Button5"
+        Me.Button5.Size = New System.Drawing.Size(52, 23)
+        Me.Button5.TabIndex = 13
+        Me.Button5.Text = "Ver"
+        Me.Button5.UseVisualStyleBackColor = True
+        '
+        'Button6
+        '
+        Me.Button6.Location = New System.Drawing.Point(308, 135)
+        Me.Button6.Name = "Button6"
+        Me.Button6.Size = New System.Drawing.Size(54, 23)
+        Me.Button6.TabIndex = 14
+        Me.Button6.Text = "Comprar"
+        Me.Button6.UseVisualStyleBackColor = True
+        '
+        'Label10
+        '
+        Me.Label10.Anchor = System.Windows.Forms.AnchorStyles.None
+        Me.Label10.AutoSize = True
+        Me.Label10.Location = New System.Drawing.Point(7, 181)
+        Me.Label10.Name = "Label10"
+        Me.Label10.Size = New System.Drawing.Size(13, 13)
+        Me.Label10.TabIndex = 15
+        Me.Label10.Text = "3"
+        '
+        'Label11
+        '
+        Me.Label11.Anchor = System.Windows.Forms.AnchorStyles.Left
+        Me.Label11.AutoSize = True
+        Me.Label11.Location = New System.Drawing.Point(30, 181)
+        Me.Label11.Name = "Label11"
+        Me.Label11.Size = New System.Drawing.Size(106, 13)
+        Me.Label11.TabIndex = 16
+        Me.Label11.Text = "Smells like teen Spirit"
+        '
+        'Label12
+        '
+        Me.Label12.Anchor = System.Windows.Forms.AnchorStyles.None
+        Me.Label12.AutoSize = True
+        Me.Label12.Location = New System.Drawing.Point(212, 181)
+        Me.Label12.Name = "Label12"
+        Me.Label12.Size = New System.Drawing.Size(28, 13)
+        Me.Label12.TabIndex = 17
+        Me.Label12.Text = "3:12"
+        '
+        'Button7
+        '
+        Me.Button7.Location = New System.Drawing.Point(250, 172)
+        Me.Button7.Name = "Button7"
+        Me.Button7.Size = New System.Drawing.Size(52, 23)
+        Me.Button7.TabIndex = 18
+        Me.Button7.Text = "Ver"
+        Me.Button7.UseVisualStyleBackColor = True
+        '
+        'Button8
+        '
+        Me.Button8.Location = New System.Drawing.Point(308, 172)
+        Me.Button8.Name = "Button8"
+        Me.Button8.Size = New System.Drawing.Size(54, 23)
+        Me.Button8.TabIndex = 19
+        Me.Button8.Text = "Comprar"
+        Me.Button8.UseVisualStyleBackColor = True
+        '
+        'Label13
+        '
+        Me.Label13.Anchor = System.Windows.Forms.AnchorStyles.None
+        Me.Label13.AutoSize = True
+        Me.Label13.Location = New System.Drawing.Point(7, 219)
+        Me.Label13.Name = "Label13"
+        Me.Label13.Size = New System.Drawing.Size(13, 13)
+        Me.Label13.TabIndex = 20
+        Me.Label13.Text = "4"
+        '
+        'Label14
+        '
+        Me.Label14.Anchor = System.Windows.Forms.AnchorStyles.Left
+        Me.Label14.AutoSize = True
+        Me.Label14.Location = New System.Drawing.Point(30, 219)
+        Me.Label14.Name = "Label14"
+        Me.Label14.Size = New System.Drawing.Size(93, 13)
+        Me.Label14.TabIndex = 21
+        Me.Label14.Text = "Fiesta Pagana 2.0"
+        '
+        'Label15
+        '
+        Me.Label15.Anchor = System.Windows.Forms.AnchorStyles.None
+        Me.Label15.AutoSize = True
+        Me.Label15.Location = New System.Drawing.Point(212, 219)
+        Me.Label15.Name = "Label15"
+        Me.Label15.Size = New System.Drawing.Size(28, 13)
+        Me.Label15.TabIndex = 22
+        Me.Label15.Text = "4:18"
+        '
+        'Button9
+        '
+        Me.Button9.Location = New System.Drawing.Point(250, 210)
+        Me.Button9.Name = "Button9"
+        Me.Button9.Size = New System.Drawing.Size(52, 23)
+        Me.Button9.TabIndex = 23
+        Me.Button9.Text = "Ver"
+        Me.Button9.UseVisualStyleBackColor = True
+        '
+        'Button10
+        '
+        Me.Button10.Location = New System.Drawing.Point(308, 210)
+        Me.Button10.Name = "Button10"
+        Me.Button10.Size = New System.Drawing.Size(54, 23)
+        Me.Button10.TabIndex = 24
+        Me.Button10.Text = "Comprar"
+        Me.Button10.UseVisualStyleBackColor = True
+        '
+        'tlpHeader
+        '
+        Me.tlpHeader.ColumnCount = 2
+        Me.tlpHeader.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 43.01075!))
+        Me.tlpHeader.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 56.98925!))
+        Me.tlpHeader.Controls.Add(Me.TableLayoutPanel3, 1, 0)
+        Me.tlpHeader.Controls.Add(Me.PictureBox1, 0, 0)
+        Me.tlpHeader.Location = New System.Drawing.Point(22, 32)
+        Me.tlpHeader.Name = "tlpHeader"
+        Me.tlpHeader.RowCount = 1
+        Me.tlpHeader.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50.0!))
+        Me.tlpHeader.Size = New System.Drawing.Size(352, 141)
+        Me.tlpHeader.TabIndex = 1
+        '
+        'TableLayoutPanel3
+        '
+        Me.TableLayoutPanel3.Anchor = System.Windows.Forms.AnchorStyles.None
+        Me.TableLayoutPanel3.ColumnCount = 1
+        Me.TableLayoutPanel3.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100.0!))
+        Me.TableLayoutPanel3.Controls.Add(Me.lblArtista, 0, 1)
+        Me.TableLayoutPanel3.Controls.Add(Me.lblTitulo, 0, 0)
+        Me.TableLayoutPanel3.Controls.Add(Me.lblPrecio, 0, 2)
+        Me.TableLayoutPanel3.Location = New System.Drawing.Point(154, 3)
+        Me.TableLayoutPanel3.Name = "TableLayoutPanel3"
+        Me.TableLayoutPanel3.RowCount = 3
+        Me.TableLayoutPanel3.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 26.27119!))
+        Me.TableLayoutPanel3.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 21.18644!))
+        Me.TableLayoutPanel3.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 51.53846!))
+        Me.TableLayoutPanel3.Size = New System.Drawing.Size(195, 135)
+        Me.TableLayoutPanel3.TabIndex = 1
+        '
+        'lblTitulo
+        '
+        Me.lblTitulo.Anchor = System.Windows.Forms.AnchorStyles.Left
+        Me.lblTitulo.AutoSize = True
+        Me.lblTitulo.Location = New System.Drawing.Point(3, 11)
+        Me.lblTitulo.Name = "lblTitulo"
+        Me.lblTitulo.Size = New System.Drawing.Size(119, 13)
+        Me.lblTitulo.TabIndex = 0
+        Me.lblTitulo.Text = "Adventure Of A Lifetime"
+        '
+        'Label2
+        '
+        Me.Label2.Anchor = System.Windows.Forms.AnchorStyles.Left
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(30, 26)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(66, 13)
+        Me.Label2.TabIndex = 25
+        Me.Label2.Text = "Canciones"
+        '
+        'lblArtista
+        '
+        Me.lblArtista.Anchor = System.Windows.Forms.AnchorStyles.Left
+        Me.lblArtista.AutoSize = True
+        Me.lblArtista.Location = New System.Drawing.Point(3, 42)
+        Me.lblArtista.Name = "lblArtista"
+        Me.lblArtista.Size = New System.Drawing.Size(51, 13)
+        Me.lblArtista.TabIndex = 1
+        Me.lblArtista.Text = "Cold Play"
+        '
+        'lblPrecio
+        '
+        Me.lblPrecio.Anchor = System.Windows.Forms.AnchorStyles.Right
+        Me.lblPrecio.AutoSize = True
+        Me.lblPrecio.Location = New System.Drawing.Point(152, 92)
+        Me.lblPrecio.Name = "lblPrecio"
+        Me.lblPrecio.Size = New System.Drawing.Size(40, 13)
+        Me.lblPrecio.TabIndex = 2
+        Me.lblPrecio.Text = "$23.99"
+        '
+        'pbCover
+        '
+        Me.pbCover.ImageStream = CType(resources.GetObject("pbCover.ImageStream"), System.Windows.Forms.ImageListStreamer)
+        Me.pbCover.TransparentColor = System.Drawing.Color.Transparent
+        Me.pbCover.Images.SetKeyName(0, "image1.jpg")
+        Me.pbCover.Images.SetKeyName(1, "image2.jpg")
+        Me.pbCover.Images.SetKeyName(2, "image3.jpg")
+        Me.pbCover.Images.SetKeyName(3, "image4.jpg")
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Location = New System.Drawing.Point(3, 3)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(136, 135)
+        Me.PictureBox1.TabIndex = 2
+        Me.PictureBox1.TabStop = False
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(383, 480)
+        Me.Controls.Add(Me.tlpHeader)
+        Me.Controls.Add(Me.TableLayoutPanel1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.TableLayoutPanel1.ResumeLayout(False)
+        Me.TableLayoutPanel1.PerformLayout()
+        Me.tlpHeader.ResumeLayout(False)
+        Me.TableLayoutPanel3.ResumeLayout(False)
+        Me.TableLayoutPanel3.PerformLayout()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents TableLayoutPanel1 As TableLayoutPanel
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label6 As Label
+    Friend WithEvents btnVer As Button
+    Friend WithEvents Button4 As Button
+    Friend WithEvents Label7 As Label
+    Friend WithEvents Label8 As Label
+    Friend WithEvents Label9 As Label
+    Friend WithEvents Button5 As Button
+    Friend WithEvents Button6 As Button
+    Friend WithEvents Label10 As Label
+    Friend WithEvents Label11 As Label
+    Friend WithEvents Label12 As Label
+    Friend WithEvents Button7 As Button
+    Friend WithEvents Button8 As Button
+    Friend WithEvents Label13 As Label
+    Friend WithEvents Label14 As Label
+    Friend WithEvents Label15 As Label
+    Friend WithEvents Button9 As Button
+    Friend WithEvents Button10 As Button
+    Friend WithEvents tlpHeader As TableLayoutPanel
+    Friend WithEvents TableLayoutPanel3 As TableLayoutPanel
+    Friend WithEvents lblTitulo As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents lblArtista As Label
+    Friend WithEvents lblPrecio As Label
+    Friend WithEvents pbCover As ImageList
+    Friend WithEvents PictureBox1 As PictureBox
+End Class
---
+++ Form1.vb
@@ -0,0 +1,75 @@
+﻿Public Class Form1
+    Private Sub Label1_Click(sender As Object, e As EventArgs)
+
+    End Sub
+
+    Private Sub Label11_Click(sender As Object, e As EventArgs) Handles Label11.Click
+
+    End Sub
+
+    Private Sub Label5_Click(sender As Object, e As EventArgs) Handles Label5.Click
+
+    End Sub
+
+
+    ''' <summary>
+    ''' Ver- Adventure Of A Lifetime
+    ''' </summary>
+    ''' <param name="sender"></param>
+    ''' <param name="e"></param>
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles btnVer.Click
+
+        lblTitulo.Text = "Adventure Of A Lifetime"
+        lblArtista.Text = "Cold Play"
+        lblPrecio.Text = "$23.99"
+        PictureBox1.Image = pbCover.Images.Item(3)
+
+
+    End Sub
+    ''' <summary>
+    ''' Ver-Sweet Child On Mine
+    ''' </summary>
+    ''' <param name="sender"></param>
+    ''' <param name="e"></param>
+    Private Sub Button5_Click(sender As Object, e As EventArgs) Handles Button5.Click
+        lblTitulo.Text = "Sweet Child On Mine"
+        lblArtista.Text = "Guns N'Roses"
+        lblPrecio.Text = "$55.79"
+        PictureBox1.Image = pbCover.Images.Item(2)
+
+
+
+    End Sub
+
+    ''' <summary>
+    ''' Ver- Smells like teen Spirit
+    ''' </summary>
+    ''' <param name="sender"></param>
+    ''' <param name="e"></param>
+    Private Sub Button7_Click(sender As Object, e As EventArgs) Handles Button7.Click
+        lblTitulo.Text = "Smells like teen Spirit"
+        lblArtista.Text = "Nirvana"
+        lblPrecio.Text = "$42.69"
+        PictureBox1.Image = pbCover.Images.Item(0)
+
+
+
+
+
+
+    End Sub
+    ''' <summary>
+    ''' Ver-Fiesta Pagana 2.0
+    ''' </summary>
+    ''' <param name="sender"></param>
+    ''' <param name="e"></param>
+    Private Sub Button9_Click(sender As Object, e As EventArgs) Handles Button9.Click
+
+        lblTitulo.Text = "Fiesta Pagana 2.0"
+        lblArtista.Text = "Mago de Oz"
+        lblPrecio.Text = "$14.59"
+        PictureBox1.Image = pbCover.Images.Item(1)
+
+
+    End Sub
+End Class
